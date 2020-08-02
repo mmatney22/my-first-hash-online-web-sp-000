de70901984cae1925d3b9337d9c1610995eedac1
Morgan Matney <morgan.matney89@gmail.com>
2020-08-02 17:44:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,3 +19,11 @@ def retrieval
 
   the_manifest["oil paintings"]
 end
+
+def adding
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
+end
