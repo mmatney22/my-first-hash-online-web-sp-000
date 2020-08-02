3d8a353fc178fdd0bbbc4ed117725b5de75322fa
Morgan Matney <morgan.matney89@gmail.com>
2020-08-02 17:41:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,28 +1,33 @@
 def my_hash
-  my_hash = {"my_name" => "Morgan Matney", "my_age" => "31"}
+  my_hash = {"my_name" => "Morgan Matney"}
 end
 
-
 def shipping_manifest
-  the_manifest={"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-  shipping_manifest["oil paintings"]
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
+
+  the_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-  shipping_manifest["muskets"] = 2
-  shipping_manifest["gun powder"] = 4
-  shipping_manifest
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
+
+  the_manifest["muskets"] = 2
+  the_manifest["gun powder"] = 4
+  the_manifest
 end
